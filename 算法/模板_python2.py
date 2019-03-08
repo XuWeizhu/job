08b5efa098b5c1611b3ccb483d87aea80db31556
ADgay <a.nice.day.hehe@gmail.com>
2019-03-08 11:27:39 +0800

    test

---
+++ 算法/模板_python2.py
@@ -0,0 +1,11 @@
+#输入输出。
+import sys
+try:
+    while True:
+        line = sys.stdin.readline().strip()
+        if line == '':
+            break
+        lines = line.split()
+        print int(lines[0]) + int(lines[1])
+except:
+    pass
